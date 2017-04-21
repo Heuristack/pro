459087d8e989cec416f2d387b814d0a3083f6144
Lance LI <heuristack@gmail.com>
2017-04-21 12:26:09 +0800

    pro: improve llvm-build.sh to rename llvm.git to llvm; [bash][llvm]

--- bash/bin/llvm-build.sh
+++ bash/bin/llvm-build.sh
@@ -4,7 +4,7 @@ cd ~/repositories;
 if [ ! -d llvm.build ]; then mkdir llvm.build; fi
 
 cd llvm.build
-cmake -DCMAKE_INSTALL_PREFIX=$INSTALL ../llvm.git
+cmake -DCMAKE_INSTALL_PREFIX=$INSTALL ../llvm
 if [ "${INSTALL}" == "" ]; then cmake --build .;
 else cmake --build . --target install; fi
 cd - &> /dev/null
