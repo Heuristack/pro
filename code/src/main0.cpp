89c6c996491cef98f8491de1f27f15adb14a7399
Lance LI <heuristack@gmail.com>
2018-05-11 16:17:48 +0800

    pro: refactor and add cpcode and mkcode for simple and relative complex challenge [code]

---
+++ code/src/main0.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <forward_list>
+#include <list>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <limits>
+#include <cmath>
+#include <numeric>
+#include <random>
+#include <istream>
+#include <ostream>
+#include <sstream>
+#include <iomanip>
+#include <exception>
+
+using namespace std;
+
+int main()
+{}
+
--- code/src/main1.cpp
+++ code/src/main1.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <limits>
+#include <cmath>
 #include <vector>
+#include <algorithm>
+
 using namespace std;
 
 int solution(vector<int> & A)
