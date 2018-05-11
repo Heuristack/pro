dfaa4d5ce35e117cd4b71a5c42aab07a9fe0edf3
Lance LI <heuristack@gmail.com>
2018-05-11 15:38:42 +0800

    pro: add codility basic main.cpp and alias to copy it to anywhere [codility]

---
+++ codility/src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int solution(vector<int> & A)
+{
+    return 0;
+}
+
+int main()
+{
+    vector<int> A = {};
+    cout << solution(A) << endl;
+}
+
