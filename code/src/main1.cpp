304d2902581fde62685f67ba434ef59e7e174d69
Lance LI <heuristack@gmail.com>
2018-05-13 13:10:57 +0800

    pro: add timing support for basic solution in seconds; remove main2.cpp [code]

--- code/src/main1.cpp
+++ code/src/main1.cpp
@@ -1,19 +1,31 @@
 #include <functional>
+#include <iterator>
 #include <algorithm>
 #include <vector>
 #include <limits>
 #include <cmath>
 #include <iostream>
-
+#include <sstream>
+#include <iomanip>
+#include <chrono>
 using namespace std;
 
 int solution()
 {
+
     return 0;
 }
 
 int main()
 {
+
+    chrono::high_resolution_clock clock;
+    auto timepoint_b = clock.now();
+
     cout << solution() << endl;
+
+    auto timepoint_e = clock.now();
+    chrono::duration<double> duration_d = {timepoint_e - timepoint_b};
+    cout << setprecision(9) << fixed << duration_d.count() << endl;
 }
 
